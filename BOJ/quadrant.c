e35a7b3e45472919e60cb788abda91d5e1a9cc7b
gye0ng-2 <dpdl9764@gmail.com>
2025-07-26 17:07:38 +0900

    Add files via upload

---
+++ BOJ/quadrant.c
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+//int main() {
+//	int a, b;
+//	scanf_s("%d",&a); //(?1000 ¡Â x ¡Â 1000; x ¡Á 0)
+//	scanf_s("%d",&b); //(?1000 ¡Â y ¡Â 1000; y ¡Á 0)
+//	if (a > 0)
+//	{
+//		if (b > 0) {
+//			printf("%d", 1);
+//		}
+//		else
+//		{
+//			printf("%d",4);
+//		}
+//	}
+//	else {
+//		if (b > 0) {
+//			printf("%d", 2);
+//		}
+//		else {
+//			printf("%d",3);
+//		}
+//	}
+//	return 0;
+//}
